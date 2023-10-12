ef29fccaf37e41b9e1753e4545b4cebe1082368d
esther-romero <202005755@est.umss.edu>
2023-10-12 10:54:41 -0400

    📌 [ADD] structure of Bet class

---
+++ src/logic/Bet.java
@@ -0,0 +1,26 @@
+package logic;
+
+public abstract class Bet {
+    protected RuleBet rule;
+
+    protected final int bonus;
+    protected final int amount;
+
+    public Bet(int bonus, int amount, RuleBet rule){
+        this.rule = rule;
+        this.bonus = bonus;
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public RuleBet getRule() {
+        return rule;
+    }
+}
