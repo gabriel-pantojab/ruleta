5b134a298df5c2beb64232ce2a7ed3a4505cd2eb
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-12 10:03:28 -0300

    ☠️[DELETE] bet class

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,5 @@ import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        Function<Integer, Boolean> f = v -> v % 2 == 0;
-        RuleBet c = new SetRule(new int[]{1, 2, 3, 4});
-        System.out.println(c.valid(new Pocket(0, 5, ValueColor.RED)));
     }
 }
