7f4f76e879f33abee3537d116a30d69cccbac2b8
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-15 14:40:52 -0300

    📌[ADD] structure of column bets

---
+++ src/logic/SecondColumnSet.java
@@ -0,0 +1,7 @@
+package logic;
+
+public class SecondColumnSet extends SetBet {
+    public SecondColumnSet(int amount) {
+        super(amount, new int[]{2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35});
+    }
+}
---
+++ src/logic/ThirdColumnSet.java
@@ -0,0 +1,7 @@
+package logic;
+
+public class ThirdColumnSet extends SetBet{
+    public ThirdColumnSet(int amount) {
+        super(amount, new int[]{3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36});
+    }
+}
