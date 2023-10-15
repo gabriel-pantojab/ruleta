d35e645f100955da13c6c2251a73f4ee3e0f9075
esther-romero <202005755@est.umss.edu>
2023-10-15 14:12:30 -0400

    📌 [ADD] structure of StreetRange class

---
+++ src/logic/StreetRange.java
@@ -0,0 +1,8 @@
+package logic;
+
+public class StreetRange extends RangeBet{
+
+    public StreetRange(int amount, int valueInit, int valueFinal){
+        super(amount, valueInit, valueFinal);
+    }
+}
