3b0a9ddc9831a5f7cdd9f24bce7ddd9cb54bbc5d
esther-romero <202005755@est.umss.edu>
2023-10-15 14:05:39 -0400

    📌 [ADD] structure of LineRange class

---
+++ src/logic/LineRange.java
@@ -0,0 +1,8 @@
+package logic;
+
+public class LineRange extends RangeBet{
+
+    public LineRange(int amount, int valueInit, int valueFinal){
+        super(amount, valueInit, valueFinal);
+    }
+}
