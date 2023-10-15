96e6157b35f1dc441908503824eb682c4a3f3e75
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-15 20:22:18 -0300

    📌[ADD] to bet in round

--- src/logic/Round.java
+++ src/logic/Round.java
@@ -1,4 +1,32 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class Round {
+    private ArrayList<Bet> bets;
+    private long totalBet;
+
+    public Round() {
+        bets = new ArrayList<Bet>();
+        totalBet = 0;
+    }
+
+    public void toBet(Bet bet) {
+        int index = existBet(bet);
+        if(index != -1) {
+            for(Chip c : bet.getChips()) {
+                bets.get(index).addChip(c);
+                totalBet += c.getValue();
+            }
+        }else {
+            bets.add(bet);
+            for(Chip c : bet.getChips()) {
+                totalBet += c.getValue();
+            }
+        }
+    }
+
+    private int existBet(Bet bet) {
+        return bets.indexOf(bet);
+    }
 }
