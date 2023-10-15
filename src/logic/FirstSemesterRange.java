374a7d4092a4d43aa5369627c9315daaf18cf14b
esther-romero <202005755@est.umss.edu>
2023-10-15 13:48:47 -0400

    📌 [ADD] structure of FirstSemesterRange class

---
+++ src/logic/FirstSemesterRange.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class FirstSemesterRange extends RangeBet{
+
+    public FirstSemesterRange(int amount){
+        super(amount, 1,18);
+    }
+
+}
--- src/logic/RangeBet.java
+++ src/logic/RangeBet.java
@@ -3,7 +3,7 @@ package logic;
  * @author Esther Romero Aguilar
  * */
 
-public class RangeBet extends Bet {
+public abstract class RangeBet extends Bet {
 
     public RangeBet(int amount, int valueInit, int valueFinal){
         super(1, amount, new RangeRule(valueInit, valueFinal));
