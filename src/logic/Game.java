721867d3b1fe698e42a284478ac8224b2e4f43ca
esther-romero <202005755@est.umss.edu>
2023-10-16 16:37:33 -0400

    💡 [UPDATE] structure game class

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -1,23 +1,35 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class Game {
     private long balance;
-    private Round[] rounds;
+    private ArrayList<Round> rounds;
     private Round currentRound;
+    private Roulette roulette;
+    private BettingGrid bettingGrid;
+    private ArrayList<Chip> chips;
 
     public Game (long balance) throws Exception {
         this.balance = balance;
-        if(balance > 1000000000) throw new Exception("The amount must be less than one billon");
+        if(balance > 1000000000000L) throw new Exception("The amount must be less than one billon");
     }
-    public void createRound(){
-        Round round = new Round();
-        currentRound = round;
-        // comenzar ronda
-        // int res = correntRound.calculeWinAmount(pocket);
-        // subtractBalance(res);
+
+    public void createNewRound(){
     }
 
     public void subtractBalance(int amount){
-        balance -= amount;
     }
+
+    public void updateBalance(){}
+
+    public void startRound(){}
+
+    public boolean toBetInRound(){
+        return false;
+    }
+
+    public void updateChips(){}
+
+
 }
