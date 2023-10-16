19753b9b5043a53a962974b85d199ee1d40394e1
esther-romero <202005755@est.umss.edu>
2023-10-15 20:39:22 -0400

    🚧 [WORK IN PROGRESS] structure Game class

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -1,4 +1,23 @@
 package logic;
 
 public class Game {
+    private long balance;
+    private Round[] rounds;
+    private Round currentRound;
+
+    public Game (long balance) throws Exception {
+        this.balance = balance;
+        if(balance > 1000000000) throw new Exception("The amount must be less than one billon");
+    }
+    public void createRound(){
+        Round round = new Round();
+        currentRound = round;
+        // comenzar ronda
+        // int res = correntRound.calculeWinAmount(pocket);
+        // subtractBalance(res);
+    }
+
+    public void subtractBalance(int amount){
+        balance -= amount;
+    }
 }
