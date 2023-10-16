3a72602a09ed44fa70af7e0931c29876c4e7f3c8
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-16 14:57:34 -0300

    📌[ADD] betting grid test

--- test/logic/BettingGridTest.java
+++ test/logic/BettingGridTest.java
@@ -22,7 +22,15 @@ class BettingGridTest {
     @Test
     public void putChipInThirtySix() {
         bettingGrid.putChip(36, Chip.FIVE);
-        int s = bettingGrid.getGrid()[0][0].getChips().size();
+        int s = bettingGrid.getGrid()[2][12].getChips().size();
         assertEquals(1, s);
+    }
+
+    @Test
+    public void putChipInFourteen() {
+        bettingGrid.putChip(14, Chip.FIVE);
+        bettingGrid.putChip(14, Chip.FIVE);
+        int s = bettingGrid.getGrid()[1][5].getChips().size();
+        assertEquals(2, s);
     }
 }
