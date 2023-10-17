4f3f532f32a0bc8a5ede4b16189d43593f165e1a
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-17 16:08:49 -0300

    💡 [UPDATE] structure game class

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -1,16 +1,18 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Game {
     private long balance;
-    private ArrayList<Round> rounds;
+    private Stack<Pocket> rounds;
     private Round currentRound;
     private Roulette roulette;
     private BettingGrid bettingGrid;
     private ArrayList<Chip> chips;
 
     public Game (long balance) throws Exception {
+        rounds = new Stack<Pocket>();
         this.balance = balance;
         if(balance > 1000000000000L) throw new Exception("The amount must be less than one billon");
     }
@@ -23,7 +25,9 @@ public class Game {
 
     public void updateBalance(){}
 
-    public void startRound(){}
+    public Pocket spinRoulette(){
+        return null;
+    }
 
     public boolean toBetInRound(){
         return false;
@@ -31,5 +35,7 @@ public class Game {
 
     public void updateChips(){}
 
-
+    public long getWinAmount(Pocket pocket){
+        return 0;
+    }
 }
