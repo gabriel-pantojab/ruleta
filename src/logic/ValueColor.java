94106672a200e545f93cb28f25536b98483bdaa7
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-18 15:42:28 -0300

    📌 [ADD] get color in value color class

--- src/logic/ValueColor.java
+++ src/logic/ValueColor.java
@@ -1,7 +1,19 @@
 package logic;
+
+import java.awt.*;
+
 /**
  * @author Gabriel Pantoja Bustamante - Esther Romero Aguilar
  */
 public enum ValueColor {
-    RED, BLACK, GREEN
+    RED(204, 25, 31), BLACK(0, 0, 0), GREEN(34, 117, 76);
+    private int r, g, b;
+    private ValueColor(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    public Color getColor() {
+        return new Color(r, g, b);
+    }
 }
