c0d5259f7475f0063c13532375185e1a8ea40170
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-18 12:29:12 -0300

    📌 [ADD] main window of game

---
+++ src/view/RouletteGame.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+
+public class RouletteGame extends JFrame {
+    public RouletteGame() {
+        setTitle("Roulette 🙂🛞");
+        setBounds(0, 0,1000, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
