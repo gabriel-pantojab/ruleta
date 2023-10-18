ca869f6e8e7c0ae3c6a6ba9be7ec134a5fe0e2ac
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-18 12:37:30 -0300

    🧨 [MERGE] ...

--- src/view/RouletteGame.java
+++ src/view/RouletteGame.java
@@ -6,6 +6,7 @@ public class RouletteGame extends JFrame {
     public RouletteGame() {
         setTitle("Roulette 🙂🛞");
         setBounds(0, 0,1000, 600);
+        setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
