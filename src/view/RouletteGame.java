b9aa83e928274bde50304f06c916dd7533d83806
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-18 12:49:58 -0300

    📌 [ADD] design table

--- src/view/RouletteGame.java
+++ src/view/RouletteGame.java
@@ -5,7 +5,9 @@ import javax.swing.*;
 public class RouletteGame extends JFrame {
     public RouletteGame() {
         setTitle("Roulette 🙂🛞");
+        TableView table = new TableView();
         setBounds(0, 0,1000, 600);
+        add(table);
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
---
+++ src/view/TableView.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TableView extends JPanel {
+    public TableView() {
+        setBackground(new Color(2, 76, 20));
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
