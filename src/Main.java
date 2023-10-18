f66e118e44fb13646a0b5400f558f4b9b9e6f185
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-18 12:29:59 -0300

    📌 [ADD] main window of game

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,12 @@
  * */
 
 import logic.*;
+import view.RouletteGame;
 
 import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        Round round = new Round();
-        Bet b1 =  new FirstDozenRange(Chip.TEN);
-        Bet b2 = new RedColor(Chip.TEN);
-        round.toBet(b1);
-        round.toBet(b2);
-        System.out.println(round.calculateWinAmount(new Pocket(0, 10, ValueColor.BLACK)));
+        new RouletteGame();
     }
 }
---
+++ src/view/RouletteGame.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.*;
+
+public class RouletteGame extends JFrame {
+    public RouletteGame() {
+        setTitle("Roulette 🙂🛞");
+        setBounds(0, 0,1000, 600);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
