2474e4e98900dad054da68effbd1a8e1f30c8936
esther-romero <202005755@est.umss.edu>
2023-10-20 22:11:03 -0400

    📌 [ADD] methods data base

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,28 @@
  * @author Esther Romero Aguilar
  * */
 
-import logic.*;
-import view.ControlGame;
-import view.RouletteGame;
-import view.TableView;
-
-import java.util.function.Function;
+import db.Conexion;
 
 public class Main {
+    static Conexion conexion = Conexion.getInstance();
+
     public static void main(String[] args) {
-        try{
-            Game game = new Game(2500L);
-            TableView table = new TableView(game.getBettingGrid());
-            ControlGame control = new ControlGame(game, table);
-            table.updateChipsAvailable(game.getChipsAvailable());
-            new RouletteGame(table);
-        }catch (Exception e){}
+        try {
+            conexion.connect();
+            //conexion.insert("Wilson", "wili");
+            conexion.selectAll();
+            conexion.deleteWhere(4);
+            conexion.selectAll();
+            conexion.closeConnection();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+//        try{
+//            Game game = new Game(2500L);
+//            TableView table = new TableView(game.getBettingGrid());
+//            ControlGame control = new ControlGame(game, table);
+//            table.updateChipsAvailable(game.getChipsAvailable());
+//            new RouletteGame(table);
+//        }catch (Exception e){}
     }
 }
