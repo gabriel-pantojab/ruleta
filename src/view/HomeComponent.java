018eae8174616f3496270fae7914c68e9a41a7e7
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-23 04:08:13 -0300

    📌 [ADD] background home

--- src/view/HomeComponent.java
+++ src/view/HomeComponent.java
@@ -1,13 +1,17 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
 
 public class HomeComponent extends JPanel {
     private JButton game;
     private JButton record;
     private Router router;
 
+
     public HomeComponent() {
+        setLayout(null);
         router = Router.getInstance();
         game = new JButton("Play");
         game.addActionListener(e -> {
@@ -19,7 +23,18 @@ public class HomeComponent extends JPanel {
         record.addActionListener(e -> {
             router.navigate("history");
         });
+        game.setFont(new Font("arial", Font.BOLD, 17));
+        game.setBounds(387, 430, 70, 40);
+        record.setBounds(487, 430, 130, 40);
+        record.setFont(new Font("arial", Font.BOLD, 17));
         add(game);
         add(record);
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Image fondoImage = Toolkit.getDefaultToolkit().getImage("src/assets" +
+                "/fondo2.jpg");
+        g.drawImage(fondoImage, 0, 0, this.getWidth(), this.getHeight(), this);
     }
 }
