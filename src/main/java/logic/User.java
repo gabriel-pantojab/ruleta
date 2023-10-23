fc02967e3d30715a51d24f67e8aba4401877ebe7
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-23 12:58:53 -0300

    📌 [ADD] auth

--- src/main/java/logic/User.java
+++ src/main/java/logic/User.java
@@ -3,10 +3,26 @@ package logic;
 public class User {
     private String nickname;
     private String password;
+    private int id;
 
     public User(String nickname, String password){
         this.nickname = nickname;
         this.password = password;
+        this.id = -1;
+    }
+
+    public User() {
+        this.nickname = null;
+        this.password = null;
+        this.id = -1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNickname() {
