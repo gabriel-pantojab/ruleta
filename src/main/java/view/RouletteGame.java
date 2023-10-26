d85908a9a6d4e61f34de3c6035d42ea59eebf1f5
GabrielPB96 <gabrieljuniorpb990@gmail.com>
2023-10-25 23:35:20 -0300

    📌 [ADD] game music

--- src/main/java/view/RouletteGame.java
+++ src/main/java/view/RouletteGame.java
@@ -2,8 +2,15 @@ package view;
 
 import logic.User;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Objects;
 
 public class RouletteGame extends JFrame {
     private Router router;
@@ -29,6 +36,35 @@ public class RouletteGame extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Thread play = new Thread(){
+            public void run() {
+                Clip clip = null;
+                try {
+                    AudioInputStream audioInputStream =
+                            AudioSystem.getAudioInputStream(Objects.requireNonNull(getClass().getResource(
+                                    "/resources/game.wav")));
+                    clip = AudioSystem.getClip();
+                    clip.open(audioInputStream);
+                    clip.loop(Clip.LOOP_CONTINUOUSLY);
+                    clip.start();
+                } catch (Exception e) {
+                    if (clip != null && clip.isOpen()) {
+                        clip.close();
+                    }
+                }
+            }
+        };
+        play.start();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                if (play.isAlive()) {
+                    play.interrupt();
+                }
+                System.exit(0);
+            }
+        });
     }
 
     private void buildRoutes() {
